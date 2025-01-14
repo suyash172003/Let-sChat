04c39b7100f0c52e14edb95fea2af3a94971479b
rocksuyash <suyash172003@gmail.com>
2025-01-14 23:40:40 +0530

    Changes in backend

--- src/main/java/org/example/websocket/Configuration/WebConfiguration.java
+++ src/main/java/org/example/websocket/Configuration/WebConfiguration.java
@@ -12,7 +12,7 @@ public class WebConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://127.0.0.1:5173/").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("http://127.0.0.1:5173").withSockJS();
 
     }
     @Override
--- src/main/java/org/example/websocket/Model/Message.java
+++ src/main/java/org/example/websocket/Model/Message.java
@@ -1,9 +1,25 @@
 package org.example.websocket.Model;
 
-import lombok.Data;
-
-@Data
 public class Message {
-    String name;
-    String content;
+    private String user;
+    private String content;
+
+    public Message(){}
+    public Message(String user, String content) {
+        this.user = user;
+        this.content = content;
+    }
+
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
